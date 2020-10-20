db38221531dd75a10be6a27c21fb305a5c0cb7cc
Ollie-Armitage <oliverrarmitage@gmail.com>
2020-10-20 16:25:14 +0100

    Removed favicon link tag in index.

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  pwa: {
+    name: 'Test',
+    iconPaths: {
+      favicon32: '(any icon file here)',
+      favicon16: '(any icon file here)',
+      appleTouchIcon: '(any icon file here)',
+      maskIcon: '(any icon file here)',
+      msTileImage: '(any icon file here)'
+    }
+  }
 }
