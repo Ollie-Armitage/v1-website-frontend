70bf5a8a9302875b3b4d45437ecba39dcdfa56fd
ollie <oliverrarmitage@gmail.com>
2021-01-04 10:10:27 +0000

    Removed Console.logs

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ let backendURL = "";
 if(process.env.VUE_APP_DEBUGMODE === "1") backendURL = "http://192.168.0.200:5000/"
 else backendURL = "https://ollie-website-backend.azurewebsites.net/"
 
-console.log(backendURL)
 
 module.exports = {
     backendURL
